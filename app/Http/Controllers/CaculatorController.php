f760bc4967d10e1ea1bb8357030c6e8dfec1d5ea
hackerpop07 <hackerpop07@gmail.com>
2019-06-25 22:34:05 +0700

    laravel

---
+++ app/Http/Controllers/CaculatorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CaculatorController extends Controller
+{
+    public function caculator(Request $request)
+    {
+        $numberA = $request->numberA;
+        $numberB = $request->numberB;
+        $kq = null;
+        if (!empty($request->addition)) {
+            $kq = $numberA + $numberB;
+        }
+        if (!empty($request->subtraction)) {
+            $kq = $numberA - $numberB;
+        }
+        if (!empty($request->multiplication)) {
+            $kq = $numberA * $numberB;
+        }
+        if (!empty($request->division) && $numberB != 0) {
+            $kq = $numberA / $numberB;
+        }
+        return view("index", compact("kq"));
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<form method="get">
+    @csrf
+    <input type="number" name="numberA">
+    <input type="number" name="numberB">
+    <br>
+    <button type="number" name="addition" value="+">Addition(+)</button>
+    <button type="number" name="subtraction" value="-">Subtraction(-)</button>
+    <button type="number" name="multiplication" value="*">Multiplication(x)</button>
+    <button type="number" name="division" value="/">Division(/)</button>
+    <br>
+    ket qua: {{$kq}}
+</form>
